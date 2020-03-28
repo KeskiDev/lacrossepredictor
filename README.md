c2b4e9e0f1d7b7641b96962b3d97dabc60ac9e57
Lyle <keskidev@gmail.com>
2020-03-28 13:55:07 -0600

    updates to get it working

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # lacrossepredictor
+
+#simple predict is a simple version to predict the winner of a lacrosse game.
+#it just compares the stats sent in and picks the one that has the most, better stats
--- simplePredict.py
+++ simplePredict.py
@@ -1,7 +1,8 @@
 import csv
 import pandas
+from random import *
 
-def compareGoalsFor(team_one_stat, team_two_stat):
+def compareNormalSituation(team_one_stat, team_two_stat):
 	if(team_one_stat > team_two_stat):
 		#team_one wins this one
 		return (1, 0)
@@ -23,33 +24,18 @@ def compareGoalsAgainst(team_one_stat, team_two_stat):
 		#both get zero point
 		return (0,0)
 
-#compareFaceOff(team_one_stat, team_two_stat)
-
-#compareManUp(team_one_stat, team_two_stat)
-
-#compareManDownKill(team_one_stat, team_two_stat)
-
-#compareSaves(team_one_stat, team_two_stat)
-
-#compareGroundBalls(team_one_stat, team_two_stat)
-
-#compareTurnovers(team_one_stat, team_two_stat)
-
-#compareCausedTurnovers(team_one_stat, team_two_stat)
-
-#compareShotPrecent(team_one_stat, team_two_stat)
-
-#compareClear(team_one_stat, team_two_stat)
-
+def RandomWinner(team1, team2):
+	x = randint(1,2)
+	if x == 1:
+		print "It's a pretty even match, but I'm guessing %s will win" % (team1)
+	else:
+		print "It's a pretty even match, but I'm guessing %s will win" % (team2)
 
 
 #read the csv
 file = open('opponents.csv')
 csv_f = csv.reader(file)
 round = 0
-
-team_one_count = 0
-team_two_count = 0
 
 #print csv_f
 for row in csv_f:
@@ -60,29 +46,48 @@ for row in csv_f:
 		team2_name, goals_for2, goals_against2, faceoff2, man_up2, man_down2, scoring_margin2, saves2, ground_balls2, turnovers2, caused_turnovers2, shot_percent2, clearing_percent2 = row
 	round += 1
 
+#goals for
+goalsFor1, goalsFor2 = compareNormalSituation(float(goals_for1), float(goals_for2))
 
-print "%s vs. %s" % (team1_name, team2_name)
+#goals against
+goalsAgainst1, goalsAgainst2 = compareGoalsAgainst(float(goals_against1), float(goals_against2))
 
-print "goals for team 1 = %s : team 2 = %s" % (goals_for1, goals_for2)
+#faceoff
+face_off1, face_off2 = compareNormalSituation(float(faceoff1), float(faceoff2))
 
-goalsFor1, goalsFor2 = compareGoalsFor(goals_for1, goals_for2)
-team_one_count += goalsFor1
-team_two_count += goalsFor2
-print "team 1 %s vs. team 2 %s" % (team_one_count, team_two_count)
-goalsAgainst1, goalsAgainst2 = compareGoalsAgainst(goals_against1, goals_against2)
-team_one_count += goalsAgainst1
-team_two_count += goalsAgainst2
-print "team 1 %s vs. team 2 %s" % (team_one_count, team_two_count)
+#manup
+manup1, manup2 = compareNormalSituation(float(man_up1), float(man_up2))
+
+#mandown
+mandown1, mandown2 = compareNormalSituation(float(man_down1), float(man_down2))
+
+#scoringMaring ?
+
+#saves
+save_team1, save_team2 = compareNormalSituation(float(saves1),float(saves2))
+
+#groundBalls
+ground1, ground2 = compareNormalSituation(float(ground_balls1), float(ground_balls2))
+
+#turnovers
+turn1, turn2 = compareNormalSituation(float(turnovers1), float(turnovers2))
+
+#caused turnovers
+caused1, caused2 = compareNormalSituation(float(caused_turnovers1), float(caused_turnovers2))
+
+#shot percent
+shot1, shot2 = compareNormalSituation(float(shot_percent1), float(shot_percent2))
+
+#clearing
+ground1, ground2 = compareNormalSituation(float(clearing_percent1), float(clearing_percent2))
 
 
+team_one_count = goalsFor1 + goalsAgainst1 + face_off1 + manup1 + save_team1 + ground1 + turn1 + caused1 + shot1 + ground1
+team_two_count = goalsFor2 + goalsAgainst2 + face_off2 + manup1 + save_team2 + ground2 + turn2 + caused2 + shot2 + ground2
 
-
-
-#data_set = pandas.read_csv('opponents.csv')
-
-
-
-
-#pull out the needed data_set
-#send it to the respective methods/variables
-#print out the predicted winner
+if(team_one_count > team_two_count):
+	print "%s should win the game" % (team1_name)
+elif(team_one_count < team_two_count):
+	print "%s should win the game" % (team2_name)
+else:
+	RandomWinner(team1_name, team2_name)
