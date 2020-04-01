7f0fc798743c8127a94b2ea9074a4aee91a15632
Lyle <keskidev@gmail.com>
2020-03-31 21:27:27 -0600

    began work on learning model method.

---
+++ predict.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+from scipy import stats
+from sklearn.cluster import DBSCAN
+from sklearn.cluster import KMeans
+from sklearn.neighbors import NearestNeighbors
+import csv
+
+#predict the winner using machine learning
