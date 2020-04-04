6978ba9dc2eaaa6b8f0a0ecd494b57eb5560999f
Lyle Olsen <lyle3olsen@outlook.com>
2020-04-04 14:48:56 -0600

    Update predict.py

--- predict.py
+++ predict.py
@@ -94,9 +94,10 @@ for round in csv_data:
     row += 1
 
 for team in range(0, len(team_stats),2):
-    print team_stats[team]
-    print team_stats[team+1]
-    #team_name_1, goals_for_1, goals_against_1, faceoff_1, man_up_1, man_down_1, scoring_margin_1, saves_1, ground_balls_1, turnovers_1, caused_turnovers_1, shot_percent_1, clearing_percent_1, team1_score, actual_winner_1 = team_stats[team]
+    #print team_stats[team]
+    #print team_stats[team+1]
+    team_name_1, goals_for_1, goals_against_1, faceoff_1, man_up_1, man_down_1, scoring_margin_1, saves_1, ground_balls_1, turnovers_1, caused_turnovers_1, shot_percent_1, clearing_percent_1, team1_score, actual_winner_1 = team_stats[team]
+    print team_name_1
     #team_name_2, goals_for_2, goals_against_2, faceoff_2, man_up_2, man_down_2, scoring_margin_2, saves_2, ground_balls_2, turnovers_2, caused_turnovers_2, shot_percent_2, clearing_percent_2, team2_score, actual_winner_2 = team_stats[team + 1]
     #team1 = [team_name_1, goals_for_1, goals_against_1, faceoff_1, man_up_1, man_down_1, scoring_margin_1, saves_1, ground_balls_1, turnovers_1, caused_turnovers_1, shot_percent_1, clearing_percent_1, actual_winner_1]
     #team2 = [team_name_2, goals_for_2, goals_against_2, faceoff_2, man_up_2, man_down_2, scoring_margin_2, saves_2, ground_balls_2, turnovers_2, caused_turnovers_2, shot_percent_2, clearing_percent_2, actual_winner_2]
@@ -106,4 +107,3 @@ for team in range(0, len(team_stats),2):
     #game_stats.append(gameInfo)
 
 
-print game_stats[1]
